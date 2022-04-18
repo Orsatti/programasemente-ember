56d6d02aa46ea93cd87311f1c9704a49ad3834b7
fernando <fernando.orsatti@gmail.com>
2022-04-18 15:58:34 -0300

    ..

--- app/analytics.js
+++ app/analytics.js
@@ -2,7 +2,6 @@ import EmberRouter from '@ember/routing/router';
 
 EmberRouter.reopen({
   notifyGoogleAnalytics: function () {
-    console.log('>_Google Analytics: ' + this.get('url'));
     if (typeof ga != 'function') {
       return;
     }

bee4e66cea8d20261c54a2a178f40b350ca1fd0b
Tiago Mingossi <tiagomingossi@gmail.com>
2022-09-19 15:35:59 -0300

    event date and link

--- app/models/event.js
+++ app/models/event.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     title: DS.attr(),
     description: DS.attr(),
-    date: DS.attr(),
+    link: DS.attr(),
+    start: DS.attr(),
+    end: DS.attr(),
 });

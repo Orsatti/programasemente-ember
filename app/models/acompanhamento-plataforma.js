2d88611704273e21208964b60702f201851107a2
TiagoMingossi <tiagomingossi@gmail.com>
2023-06-13 12:55:38 -0300

    .

--- app/models/acompanhamento-plataforma.js
+++ app/models/acompanhamento-plataforma.js
@@ -3,10 +3,13 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     acompanhamentoPessoasPlataforma: DS.hasMany('acompanhamento-pessoa-plataforma', { async: true }),
     acompanhamentoSistemasPlataforma: DS.hasMany('acompanhamento-sistema-plataforma', { async: true }),
+    acompanhamentoAtividadesPlataforma: DS.hasMany('acompanhamento-atividade-instituicao', { async: true }),
     nrAlunos: DS.attr(),
     nrProfessores: DS.attr(),
     nrSecretarias: DS.attr(),
     nrMarketings: DS.attr(),
     nrCooredenadores: DS.attr(),
     nrGestores: DS.attr(),
+    professoresFiltered: DS.attr(),
+    alunosFiltered: DS.attr(),
 });

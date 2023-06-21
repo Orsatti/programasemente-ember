ec97be3d5797eb1ce29977445ffdbef176fcad86
TiagoMingossi <tiagomingossi@gmail.com>
2023-06-20 21:18:10 -0300

    .

--- app/models/acompanhamento-pessoa-plataforma.js
+++ app/models/acompanhamento-pessoa-plataforma.js
@@ -4,12 +4,14 @@ export default DS.Model.extend({
     pessoa: DS.belongsTo('pessoa', { async: true }),
     conteudoPessoa: DS.belongsTo('conteudo-pessoa', { async: true }),
     nrVideosPreparacao: DS.attr(),
-    nrVideos: DS.attr(),
+    nrVideosAlunos: DS.attr(),
+    nrTarefasAlunos: DS.attr(),
     nrAplicacaoPlataformaAulas: DS.attr(),
     acessos: DS.attr(),
     ultimoacesso: DS.attr(),
     role: DS.attr(),
     name: DS.attr(),
+    userName: DS.attr(),
     lastAccess: Ember.computed('ultimoacesso', function() {
         return moment(this.get('ultimoacesso'), 'X').format('DD/MM/YYYY');
     }),
